a634121502e31d5bef7263f33dbe710c589d3af4
nycode802 <nycode802@users.noreply.github.com>
2015-09-20 19:52:30 -0400

    Update main.php

--- New Project/main.php
+++ New Project/main.php
@@ -4,7 +4,24 @@
 </head>
 <body>
 <?php
-   echo "<h1>Hello, PHP!</h1>";
+   $Price = 50.32 ;
+   $Discount = 0 ;
+   $Total = 0 ;
+   $Sub_Total = 0;
+   $Items = 10;
+  function Disc10(){
+   	$Discount = 10;
+   	$Price / $Discount = $Sub_Total ; 
+   	
+   	echo $Sub_Total;
+   }
+   if ($Price >= 20 && $Items >= 10 ); {
+   	
+   	Disc10()
+   	
+   	
+   }
+   
 ?>
 </body>
 </html>
